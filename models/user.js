07c50cfd3b98d5f398f8ea8315c1fbbff1719368
torreseam <615694479+torreseam@users_noreply.github.com>
2020-06-09 18:45:44 -0500

    password hash code update

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
+//Other Dependancies 
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+//bcrypt dependancies
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model { }
@@ -44,13 +47,25 @@ User.init(
             }
         }
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'user'
-    }
+   {
+      hooks: {
+         // set up beforeCreate lifecycle "hook" functionality
+         async beforeCreate(newUserData) {
+           newUserData.password = await bcrypt.hash(newUserData.password, 10);
+           return newUserData;
+         },
+         // set up beforeUpdate lifecycle "hook" functionality
+         async beforeUpdate(updatedUserData) {
+           updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+           return updatedUserData;
+         }
+       },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+   }
 );
 
 module.exports = User;
