8a848671dc08bb7a400ea432835b107af9004b14
torreseam <615694479+torreseam@users_noreply.github.com>
2020-06-10 23:03:43 -0500

    update post routes for db get all users in db, single users and create post requests

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,14 @@
 
 // THIS FILE will serve as a means to collect all of the API routes and package them up
-
+//dependancies from Post, User.js folders 
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
+
 
 module.exports = router;
