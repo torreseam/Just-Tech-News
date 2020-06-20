2278585b2a35542eccf3d10093e1354619f8eb1c
torreseam <615694479+torreseam@users_noreply.github.com>
2020-06-19 22:08:21 -0500

    optimize code and testing

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+//Dependancies
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/helper');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -28,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Handlebars.js as your app's template engine
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
